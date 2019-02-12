0659d49c1d824115a9ef29a18fc1ee6ecd0047ce
Nathan Davies <ndavies@turnitin.com>
2019-02-12 20:46:53 +0000

    basic routing examples

--- nostdlib/code/routing/main.go
+++ nostdlib/code/routing/main.go
@@ -2,36 +2,57 @@ package main
 
 import (
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"log"
 	"net/http"
 	"strings"
 )
 
-func indexGet(w http.ResponseWriter, r *http.Request) {
+func indexHdlr(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to your first web server!")
 }
 
-func nameGet(w http.ResponseWriter, r *http.Request) {
+func nameHdlr(w http.ResponseWriter, r *http.Request) {
 	// name is the last part of the path
 	fmt.Fprintf(w, "From the path: %q", strings.Split(r.URL.Path, "/")[2])
 
 }
 
-func ageLocGet(w http.ResponseWriter, r *http.Request) {
-	// name is the last part of the path
+func ageLocHdlr(w http.ResponseWriter, r *http.Request) {
+	// get the values from the path - inherently buggy as is
 	parts := strings.Split(r.URL.Path, "/")
 	fmt.Fprintf(w, "Age: %s; Loc: %s", parts[3], parts[4])
-
 }
 
+func namedAgeLocGET(w http.ResponseWriter, r *http.Request,  ps httprouter.Params){
+	fmt.Fprintf(w, "Age: %s; Loc: %s", ps.ByName("age"), ps.ByName("loc"))
+}
+
+func namedAgeLocPUT(w http.ResponseWriter, r *http.Request,  ps httprouter.Params){
+	fmt.Fprintf(w, "PUTTING Age: %s; Loc: %s", ps.ByName("age"), ps.ByName("loc"))
+}
 func main() {
-	http.HandleFunc("/", indexGet) // set router
-	http.HandleFunc("/name/", nameGet) // get one thing from the path
-	http.HandleFunc("/age/location/", ageLocGet) // get two things from the path
+	// Standard library only
+	http.HandleFunc("/", indexHdlr)
+	http.HandleFunc("/name/", nameHdlr)           // get one thing from the path
+	http.HandleFunc("/age/location/", ageLocHdlr) // get two things from the path
+	// err := http.ListenAndServe(":9090", nil)
+	// if err != nil {
+	// 	log.Fatal("ListenAndServe: ", err)
+	// }
+
 	// Not very RESTful for those paths.
 	// Would rather have /age/:age/location/:location
-	err := http.ListenAndServe(":9090", nil) // set listen port
+	// Using httprouter
+	router := httprouter.New()
+	router.GET("/age/:age/location/:loc", namedAgeLocGET) // using named path components
+	router.PUT("/age/:age/location/:loc", namedAgeLocPUT)
+
+	log.Print("Serving on port 9090")
+	err := http.ListenAndServe(":9090", router)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
+
+// NOTE: For TESTING cover testify and httpmock
